b261043e036a48e8719f7c4b98557c9b2266f2b3
asisrasyid <118648153+asisrasyid@users.noreply.github.com>
2025-04-08 16:24:44 +0700

    update index
    update all html

--- service-worker.js
+++ service-worker.js
@@ -9,12 +9,14 @@ const urlsToCache = [
 	'assets/css/nice-select.css',
 	'assets/css/style.css',
 	'assets/css/responsive.css',
-  'assets/js/bootstrap.bundle.min.js',
+  	'assets/js/bootstrap.bundle.min.js',
 	'assets/js/jquery-3.5.1.min.js',
 	'assets/js/jquery-ui.min.js',
 	'assets/js/slick.min.js',
 	'assets/js/jquery.nice-select.min.js',
 	'assets/js/app.js',
+	'/assets/sweetalert2/sweetalert2.min.js',
+  '/assets/sweetalert2/sweetalert2.min.css',
   // Tambahkan file lain seperti logo, script, dll.
 ];
 
