45df1d142cfe28d316595e001c58d8d2cd5050b0
asisrasyid <118648153+asisrasyid@users.noreply.github.com>
2025-04-08 10:36:18 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'my-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'assets/css/bootstrap.min.css',
+	'assets/css/jquery-ui.css',
+	'assets/css/slick.css',
+	'assets/css/line-awesome.css',
+	'assets/css/nice-select.css',
+	'assets/css/style.css',
+	'assets/css/responsive.css',
+  'assets/js/bootstrap.bundle.min.js',
+	'assets/js/jquery-3.5.1.min.js',
+	'assets/js/jquery-ui.min.js',
+	'assets/js/slick.min.js',
+	'assets/js/jquery.nice-select.min.js',
+	'assets/js/app.js',
+  // Tambahkan file lain seperti logo, script, dll.
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Installed');
+});
+
+self.addEventListener('fetch', function(event) {
+  // Bisa disesuaikan jika ingin offline support
+});
